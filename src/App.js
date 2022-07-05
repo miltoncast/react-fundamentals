6d98c1f9bdfb214787a0aa5bf2f251641433b011
John Milton Castillo <jomilcastle@gmail.com>
2022-07-05 19:01:11 +0800

    done with part 3

--- src/App.js
+++ src/App.js
@@ -4,24 +4,59 @@ import Card from './components/Card.js';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [name, setName] = useState('Bulbasaur');
+  const [showCard, setShowCard] = useState(false);
 
+  const handleInputChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
 
   return (
     <div className="App">
       <div className="formField">
-        <button>Add</button>
-        <p>0</p>
+        <button onClick={(e)=>{
+          setCount(count + 1)
+        }}>
+           Add
+        </button>
+        <p>{count}</p>
       </div>
+
       <div className="formField">
         <h3>Input field</h3>
-        <input value={1} />
+        <input value={text} onChange={handleInputChange}/>
+      </div>
+
+      <div className="formField">
+        <h3>Name</h3>
+        <input value={name} onChange={handleNameChange}/>
+      </div>
+      <div className="formField">
+        <button onClick={(e)=>{
+            setShowCard(!showCard)
+          }} 
+          style={{
+            color: showCard ? 'green' : 'grey'
+          }}
+        >
+            Toggle Card
+        </button>
       </div>
       <div className="grid">
-        <Card 
-          image='bulbasaur.jpg'
-          name ='Bulbasaur'
-          desc ='A grass type pokemon'
-        />
+        {
+          showCard ? (
+          <Card 
+            image='bulbasaur.jpg'
+            name ={name}
+            desc ={text}
+          />
+          ) : (false)
+        }
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+
 const Card = ({
   image,
   name,
   desc
 }) => {
  
+
+  useEffect(()=> {
+    console.log('mounted')
+
+    return () => {
+      console.log('unmounted')
+    }
+  }, [name]);
+
+
   return (
     <div className="card">
       <img src={`images/${image}`} alt={image}/>
