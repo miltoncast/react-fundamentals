9bad43044a31ddbfe21905aba4d16a50155f2a7d
John Milton Castillo <jomilcastle@gmail.com>
2022-07-05 09:16:28 +0800

    start of part 3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import './App.css';
-import {pokemon} from './pokemon.js';
-
-
-function Card({
-  image,
-  name,
-  desc
-}) {
-
-  return (
-    <div className="card">
-      <img src={`images/${image}`} alt={image}/>
-      <h2>{name}</h2>
-      <p>{desc}</p>
-    </div>
-  )
-}
+import { useState } from 'react';
+import Card from './components/Card.js';
 
 function App() {
+  const [count, setCount] = useState(0);
+
 
   return (
-    <div className="App grid">
-      {pokemon.map((item)=> {
-        return <Card
-          image={item.image}
-          name={item.name}
-          desc={item.desc}
+    <div className="App">
+      <div className="formField">
+        <button>Add</button>
+        <p>0</p>
+      </div>
+      <div className="formField">
+        <h3>Input field</h3>
+        <input value={1} />
+      </div>
+      <div className="grid">
+        <Card 
+          image='bulbasaur.jpg'
+          name ='Bulbasaur'
+          desc ='A grass type pokemon'
         />
-      })}
+      </div>
     </div>
   );
 }
